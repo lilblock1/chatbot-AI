5e0bfc47454b89ed6fe016fbfb67265782675cf3
lilblock1 <58580672+lilblock1@users.noreply.github.com>
2021-03-17 11:58:07 -0500

    Add files via upload

---
+++ Chatbot AI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           Console.Title = "Your AI friend";
+           Console.ForegroundColor = ConsoleColor.DarkCyan;
+           Console.WindowHeight = 20;
+           Console.WindowWidth = 40;
+
+            Console.WriteLine("What's your name?");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + userName + ", nice to meet you!");
+
+            Console.WriteLine("How are you to day " + userName + ".");
+
+            string input = Console.ReadLine();
+
+if (input.ToLower() == "good"){
+Console.WriteLine("That's good what are your plans for today");
+}
+if (input.ToLower() == "bad")
+{
+Console.WriteLine("what's wrong");
+}
+
+     Console.ReadLine();
+
+            Console.ReadKey();
+        }
+    }
+}
